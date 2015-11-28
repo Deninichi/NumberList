037da124e888319e012494c4a77e06ce6a301c27
Denis <deninichi@gmail.com>
2015-11-28 19:27:27 +0400

    Начало работы над графическим окном выбора файла

--- src/InputFile.java
+++ src/InputFile.java
@@ -1,3 +1,7 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
@@ -5,11 +9,50 @@ import java.util.List;
 /**
  * Created by Пользователь on 28.11.2015.
  */
-public class InputFile {
+public class InputFile extends JFrame {
     private String InFile;
     private List<String> numbers;
+    private File file;
 
     public InputFile () throws IOException {
+//        super("ConvNumber - Форматирование телефонных номеров.");
+//        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+//        JPanel panel = new JPanel();
+//        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+
+//        panel.add(Box.createVerticalGlue());
+
+//        final JLabel label = new JLabel("Укажите список телефонных номеров в текстовом файле.");
+//        label.setAlignmentX(CENTER_ALIGNMENT);
+//        panel.add(label);
+
+//        panel.add(Box.createRigidArea(new Dimension(10, 10)));
+
+//        JButton button = new JButton("Выбрать файл");
+//        button.setAlignmentX(CENTER_ALIGNMENT);
+
+//        button.addActionListener(new ActionListener() {
+//            public void actionPerformed(ActionEvent e) {
+//                JFileChooser fileopen = new JFileChooser();
+//                int ret = fileopen.showDialog(null, "Открыть файл");
+//                if (ret == JFileChooser.APPROVE_OPTION) {
+//                    File file = fileopen.getSelectedFile();
+//                    label.setText(file.getName());
+//                }
+//            }
+//        });
+
+//        panel.add(button);
+//        panel.add(Box.createVerticalGlue());
+//        getContentPane().add(panel);
+
+//        setPreferredSize(new Dimension(460, 120));
+//        pack();
+//        setLocationRelativeTo(null);
+//        setVisible(true);
+
+
         BufferedReader readerstream = new BufferedReader(new InputStreamReader(System.in));
         InFile = readerstream.readLine();
 
@@ -30,4 +73,8 @@ public class InputFile {
     public List<String> getNumbers() {
         return numbers;
     }
+
+//    public File getFile() {
+//        return file;
+//    }
 }
